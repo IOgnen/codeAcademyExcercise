8d92df14a534c636baccc832b82bd5c77a141bef
IOgnen <90794671+IOgnen@users.noreply.github.com>
2021-09-29 18:33:34 +0200

    Update IntNiza.java

--- Main/Mini/IntNiza.java
+++ Main/Mini/IntNiza.java
@@ -1,4 +1,4 @@
-package Mini;
+package main;
 
 import java.util.Arrays;
 import java.util.Scanner;
@@ -6,45 +6,21 @@ import java.util.Scanner;
 public class IntNiza {
 	public static void main(String[] args) {
 		
-		int broj;
-		int x;
-		int najgolem=0;
-		int najmal=10000;
+		int y[] = {53,32,19,79,25,5,47};
 		
+		Integer min = y[0];
+		Integer max = y[0];
 		
-		System.out.print("Vnesete dolzina na niza: ");
-		
-		Scanner vnes = new Scanner(System.in);
-		broj = vnes.nextInt();
-		
-		int y[] = new int[broj];
-		
-		System.out.print("Vnesete gi broevite: ");
-		
-		for(x=0 ; x<broj ; x++){
-			y[x] = vnes.nextInt();
+		for(int x=0 ; x<y.length ; x++){
 			
-			if (najgolem < y[x])
-				najgolem = y[x];
+			if (max < y[x])
+				max = y[x];
 			
-			if (najmal > y[x])
-				najmal = y[x];
+			if (min > y[x])
+				min = y[x];
 		}
 		
-		for(int i: y) {	
-			System.out.print(i + " ");
-		}
-		System.out.println(" ");
-		
-		System.out.println("Najgolem broj e: " + najgolem);
-		System.out.println("Najgolem broj e: " + najmal);
-	
-		Arrays.sort(y);
-		
-		System.out.print("Nagolemi 3 broevi se: ");
-		
-		for (x=broj-3 ; x<broj ; x++) {
-			System.out.print(y[x]+ " ");
-		}
+		System.out.println("Najgolem broj e: " + max);
+		System.out.println("Najmal broj e: " + min);
 	}
 }
